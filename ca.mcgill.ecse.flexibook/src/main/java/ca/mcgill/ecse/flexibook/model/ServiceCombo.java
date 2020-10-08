df9fb86b7be42917982de11971b86bfefa3241f8
loucadufault <louca.dufault@gmail.com>
2020-10-08 16:23:46 -0400

    regenearated model code based on common-base updates to domain model (#9)

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/model/ServiceCombo.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/model/ServiceCombo.java
@@ -20,7 +20,7 @@ public class ServiceCombo extends BookableService
   // CONSTRUCTOR
   //------------------------
 
-  public ServiceCombo(String aName, FlexiBook aFlexiBook, ComboItem aMainService, ComboItem... allServices)
+  public ServiceCombo(String aName, FlexiBook aFlexiBook, ComboItem aMainService)
   {
     super(aName, aFlexiBook);
     if (!setMainService(aMainService))
@@ -28,11 +28,6 @@ public class ServiceCombo extends BookableService
       throw new RuntimeException("Unable to create ServiceCombo due to aMainService. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
     }
     services = new ArrayList<ComboItem>();
-    boolean didAddServices = setServices(allServices);
-    if (!didAddServices)
-    {
-      throw new RuntimeException("Unable to create ServiceCombo, must have at least 2 services. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
   }
 
   //------------------------
@@ -99,96 +94,54 @@ public class ServiceCombo extends BookableService
   {
     return 2;
   }
-  /* Code from template association_AddManyToManyMethod */
+  /* Code from template association_AddMandatoryManyToOne */
+  public ComboItem addService(boolean aMandatory, Service aService)
+  {
+    ComboItem aNewService = new ComboItem(aMandatory, aService, this);
+    return aNewService;
+  }
+
   public boolean addService(ComboItem aService)
   {
     boolean wasAdded = false;
     if (services.contains(aService)) { return false; }
-    services.add(aService);
-    if (aService.indexOfServiceCombo(this) != -1)
+    ServiceCombo existingServiceCombo = aService.getServiceCombo();
+    boolean isNewServiceCombo = existingServiceCombo != null && !this.equals(existingServiceCombo);
+
+    if (isNewServiceCombo && existingServiceCombo.numberOfServices() <= minimumNumberOfServices())
     {
-      wasAdded = true;
+      return wasAdded;
+    }
+    if (isNewServiceCombo)
+    {
+      aService.setServiceCombo(this);
     }
     else
     {
-      wasAdded = aService.addServiceCombo(this);
-      if (!wasAdded)
-      {
-        services.remove(aService);
-      }
+      services.add(aService);
     }
+    wasAdded = true;
     return wasAdded;
   }
-  /* Code from template association_AddMStarToMany */
+
   public boolean removeService(ComboItem aService)
   {
     boolean wasRemoved = false;
-    if (!services.contains(aService))
+    //Unable to remove aService, as it must always have a serviceCombo
+    if (this.equals(aService.getServiceCombo()))
     {
       return wasRemoved;
     }
 
+    //serviceCombo already at minimum (2)
     if (numberOfServices() <= minimumNumberOfServices())
     {
       return wasRemoved;
     }
 
-    int oldIndex = services.indexOf(aService);
-    services.remove(oldIndex);
-    if (aService.indexOfServiceCombo(this) == -1)
-    {
-      wasRemoved = true;
-    }
-    else
-    {
-      wasRemoved = aService.removeServiceCombo(this);
-      if (!wasRemoved)
-      {
-        services.add(oldIndex,aService);
-      }
-    }
+    services.remove(aService);
+    wasRemoved = true;
     return wasRemoved;
-  }
-  /* Code from template association_SetMStarToMany */
-  public boolean setServices(ComboItem... newServices)
-  {
-    boolean wasSet = false;
-    ArrayList<ComboItem> verifiedServices = new ArrayList<ComboItem>();
-    for (ComboItem aService : newServices)
-    {
-      if (verifiedServices.contains(aService))
-      {
-        continue;
-      }
-      verifiedServices.add(aService);
-    }
-
-    if (verifiedServices.size() != newServices.length || verifiedServices.size() < minimumNumberOfServices())
-    {
-      return wasSet;
-    }
-
-    ArrayList<ComboItem> oldServices = new ArrayList<ComboItem>(services);
-    services.clear();
-    for (ComboItem aNewService : verifiedServices)
-    {
-      services.add(aNewService);
-      if (oldServices.contains(aNewService))
-      {
-        oldServices.remove(aNewService);
-      }
-      else
-      {
-        aNewService.addServiceCombo(this);
-      }
-    }
-
-    for (ComboItem anOldService : oldServices)
-    {
-      anOldService.removeServiceCombo(this);
-    }
-    wasSet = true;
-    return wasSet;
   }
   /* Code from template association_AddIndexControlFunctions */
   public boolean addServiceAt(ComboItem aService, int index)

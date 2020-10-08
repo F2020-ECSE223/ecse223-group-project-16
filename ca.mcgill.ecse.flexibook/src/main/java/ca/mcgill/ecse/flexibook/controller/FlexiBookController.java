1027191f8161da1903adc0d0f4be10dca09f241b
Louca Dufault <louca.dufault@gmail.com>
2020-10-08 18:50:24 -0400

    improved the controller methods to better match test scenarios in terms of method signature and thrown exceptions, organized the methods in order of CRUD, made non-API methods private

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/controller/FlexiBookController.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/controller/FlexiBookController.java
@@ -14,13 +14,22 @@ public class FlexiBookController {
 	 * @return the created Customer account
 	 * 
 	 * @throws IllegalArgumentException if any of the username or password are null
-	 * @throws InvalidInputException if any of the username or password are empty or whitespace, or if the username already exists
+	 * @throws InvalidInputException if:
+	 * - any of the username or password are empty or whitespace
+	 * - the logged in User account is the Owner account
+	 * - the username already exists
 	 */
 	public static Customer createCustomerAccount(String username, String password) throws InvalidInputException {
+		FlexiBook flexiBook = FlexiBookApplication.getFlexiBook();
 		validateCustomerAccountUsername(username);
 		validateUserAccountPassword(password);
+		
+		if (getLoggedInUserAccount() == flexiBook.getOwner()) {
+			throw new InvalidInputException("You must log out of the owner account before creating a customer account");
+		}
+		
 		try {
-			return new Customer(username, password, FlexiBookApplication.getFlexiBook());
+			return new Customer(username, password, flexiBook);
 		} catch (RuntimeException e) { // this conceals other RuntimeExceptions that may occur during Customer construction
 			throw new InvalidInputException("The username already exists");
 		}
@@ -44,7 +53,14 @@ public class FlexiBookController {
 		}
 	}
 	
-	public static Customer getCustomerByUsername(String username) {
+	private static User getUserByUsername(String username) {
+		if (username.equals("owner")) {
+			return FlexiBookApplication.getFlexiBook().getOwner();
+		}
+		return getCustomerByUsername(username);
+	}
+	
+	private static Customer getCustomerByUsername(String username) {
 		for (Customer customer : FlexiBookApplication.getFlexiBook().getCustomers()) {
 			if (customer.getUsername().equals(username)) {
 				return customer;
@@ -53,8 +69,68 @@ public class FlexiBookController {
 		return null;
 	}
 	
-	public static User getLoggedInUser() {
+	public static User getLoggedInUserAccount() {
 		return null;
+	}
+	
+	/**
+	 * @author louca
+	 * 
+	 * @param customer to update
+	 * @param newUsername with which to update the Customer account
+	 * @return whether or not the Customer account username was updated
+	 * 
+	 * @throws InvalidInputException if the newUsername is empty or whitespace, or if the newUsername is not available
+	 */
+	public static boolean updateCustomerAccountUsername(Customer customer, String newUsername) throws InvalidInputException {
+		validateCustomerAccountUsername(newUsername);
+		if (customer.setUsername(newUsername)) {
+			return true;
+		}
+		throw new InvalidInputException("Username not available");
+	}
+	
+	/**
+	 * @author louca
+	 * 
+	 * @param user to update
+	 * @param newPassword with which to update the User account
+	 * @return whether or not the User account password was updated
+	 * 
+	 * @throws InvalidInputException if the newPassword is empty or whitespace
+	 */
+	public static boolean updateUserAccountPassword(User user, String newPassword) throws InvalidInputException {
+		validateUserAccountPassword(newPassword);
+		user.setPassword(newPassword);
+		return true;
+	}
+	
+	/**
+	 * @author louca
+	 * 
+	 * @param user of the User account to update
+	 * @param newUsername with which to update the User account
+	 * @param newPassword with which to update the User account
+	 * @return whether or not the User account was updated
+	 * 
+	 * @throws InvalidInputException if 
+	 * - the newUsername is empty or whitespace
+	 * - the newPassword is empty or whitespace
+	 * - the newUsername is not available
+	 * - the User by the given username is the Owner, and the newUsername is not "owner"
+	 */
+	public static boolean updateUserAccount(String username, String newUsername, String newPassword) throws InvalidInputException {
+		User userToUpdate = getUserByUsername(username);
+		
+		if (userToUpdate instanceof Customer) {
+			return updateCustomerAccountUsername((Customer) userToUpdate, newUsername) 
+					&& updateUserAccountPassword(userToUpdate, newPassword);
+		} else { // instanceof Owner
+			if (!newUsername.equals("owner")) {
+				throw new InvalidInputException("Changing username of owner is not allowed");
+			}
+			return updateUserAccountPassword(userToUpdate, newPassword);
+		}
 	}
 	
 	/**
@@ -65,7 +141,7 @@ public class FlexiBookController {
 	 */
 	public static boolean deleteCustomer(String username) {
 		Customer customerToDelete = getCustomerByUsername(username);
-		if (customerToDelete != null && customerToDelete == getLoggedInUser()) {
+		if (customerToDelete != null && customerToDelete == getLoggedInUserAccount()) {
 			deleteAllCustomerAppointments(customerToDelete);
 			customerToDelete.delete();
 			return true;
@@ -83,51 +159,5 @@ public class FlexiBookController {
 		for (Appointment appointment : allCustomerAppointments) {
 			deleteAppointment(appointment);
 		}
-	}
-	
-	/**
-	 * @author louca
-	 * 
-	 * @param customer to update
-	 * @param newUsername with which to update the Customer account
-	 * @return the updated Customer account
-	 * 
-	 * @throws InvalidInputException if the newUsername is empty or whitespace, or if the newUsername already exists
-	 */
-	public static Customer updateCustomerAccountUsername(Customer customer, String newUsername) throws InvalidInputException {
-		validateCustomerAccountUsername(newUsername);
-		customer.setUsername(newUsername);
-		return customer;
-	}
-	
-	/**
-	 * @author louca
-	 * 
-	 * @param user to update
-	 * @param newPassword with which to update the User account
-	 * @return the updated User account
-	 * 
-	 * @throws InvalidInputException if the newPassword is empty or whitespace
-	 */
-	public static User updateUserAccountPassword(User user, String newPassword) throws InvalidInputException {
-		validateUserAccountPassword(newPassword);
-		user.setPassword(newPassword);
-		return user;
-	}
-	
-	/**
-	 * @author louca
-	 * 
-	 * @param customer to update
-	 * @param newUsername with which to update the Customer account
-	 * @param newPassword with which to update the Customer account
-	 * @return
-	 * 
-	 * @throws InvalidInputException
-	 */
-	public static Customer updateCustomerAccount(Customer customer, String newUsername, String newPassword) throws InvalidInputException {
-		updateCustomerAccountUsername(customer, newUsername);
-		updateUserAccountPassword(customer, newPassword);
-		return customer;
 	}
 }

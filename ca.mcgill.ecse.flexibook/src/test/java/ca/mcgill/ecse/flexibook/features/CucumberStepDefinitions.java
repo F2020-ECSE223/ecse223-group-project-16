4ed2c5b78f1ee3c695a678ac7c56722f7f83a6f9
Louca Dufault <louca.dufault@gmail.com>
2020-10-09 00:47:56 -0400

    added the boilerplate step definitions for feature set 1

--- ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/features/CucumberStepDefinitions.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
 
+import ca.mcgill.ecse.flexibook.application.FlexiBookApplication;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
@@ -14,7 +15,60 @@ import io.cucumber.java.en.When;
 
 public class CucumberStepDefinitions {
 
-	// TODO you can add step implementations here
+	@Given("a Flexibook system exists")
+	public void a_flexibook_system_exists() {
+	    assertTrue(FlexiBookApplication.getFlexiBook() != null);
+	}
+
+	@Given("an owner account exists in the system with username {string} and password {string}")
+	public void an_owner_account_exists_in_the_system_with_username_and_password(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("the following customers exist in the system:")
+	public void the_following_customers_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("the account with username {string} has pending appointments")
+	public void the_account_with_username_has_pending_appointments(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("the user is logged in to an account with username {string}")
+	public void the_user_is_logged_in_to_an_account_with_username(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("the user tries to delete account with the username {string}")
+	public void the_user_tries_to_delete_account_with_the_username(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("the account with the username {string} does not exist")
+	public void the_account_with_the_username_does_not_exist(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("all associated appointments of the account with the username {string} shall not exist")
+	public void all_associated_appointments_of_the_account_with_the_username_shall_not_exist(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("the user shall be logged out")
+	public void the_user_shall_be_logged_out() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
 
 }
 

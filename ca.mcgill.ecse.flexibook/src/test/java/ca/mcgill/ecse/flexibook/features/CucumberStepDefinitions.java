c7846007419cd4202dd62fa821643fa32d09eb89
anotherLostKitten <eggismagicchickensquared@gmail.com>
2020-10-12 19:24:41 -0400

    javadocs eww

--- ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/features/CucumberStepDefinitions.java
@@ -294,74 +294,114 @@ public class CucumberStepDefinitions {
 	//================================================================================
     // DefineServiceCombo
     //================================================================================
-
+	/**
+	 * @author theodore
+	 */
 	@Given("an owner account exists in the system")
     public void an_owner_account_exists_in_the_system() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
+	/**
+	 * @author theodore
+	 */
     @Given("a business exists in the system")
     public void a_business_exists_in_the_system() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @Given("the following services exist in the system:")
-    public void the_following_services_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
-    }
+    /**
+	 * @author theodore
+	 * @param string username
+	 */
     @Given("the Owner with username {string} is logged in")
     public void the_owner_with_username_is_logged_in(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string combo name
+	 * @param string2 main service
+	 * @param string3 services
+	 * @param string4 mandatory
+	 */
     @When("{string} initiates the definition of a service combo {string} with main service {string}, services {string} and mandatory setting {string}")
     public void initiates_the_definition_of_a_service_combo_with_main_service_services_and_mandatory_setting(String string, String string2, String string3, String string4, String string5) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string combo name
+	 */
     @Then("the service combo {string} shall exist in the system")
     public void the_service_combo_shall_exist_in_the_system(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string combo name
+	 * @param string2 services
+	 * @param string3 mandatory
+	 */
     @Then("the service combo {string} shall contain the services {string} with mandatory setting {string}")
     public void the_service_combo_shall_contain_the_services_with_mandatory_setting(String string, String string2, String string3) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string combo name
+	 * @param string2 main service
+	 */
     @Then("the main service of the service combo {string} shall be {string}")
     public void the_main_service_of_the_service_combo_shall_be(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string combo name
+	 * @param string2 main service
+	 */
     @Then("the service {string} in service combo {string} shall be mandatory")
     public void the_service_in_service_combo_shall_be_mandatory(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string number of combos
+	 */
     @Then("the number of service combos in the system shall be {string}")
     public void the_number_of_service_combos_in_the_system_shall_be(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string error content
+	 */
     @Then("an error message with content {string} shall be raised")
     public void an_error_message_with_content_shall_be_raised(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string combo name
+	 */
     @Then("the service combo {string} shall not exist in the system")
     public void the_service_combo_shall_not_exist_in_the_system(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string username
+	 */
     @Given("Customer with username {string} is logged in")
     public void customer_with_username_is_logged_in(String string) {
         // Write code here that turns the phrase above into concrete actions
@@ -372,6 +412,10 @@ public class CucumberStepDefinitions {
     // DeleteServiceCombo
     //================================================================================
 	
+    /**
+	 * @author theodore
+	 * @param dataTable service combos
+	 */
     @Given("the following service combos exist in the system:")
     public void the_following_service_combos_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
         // Write code here that turns the phrase above into concrete actions
@@ -383,27 +427,30 @@ public class CucumberStepDefinitions {
         // For other transformations you can register a DataTableType.
         throw new io.cucumber.java.PendingException();
     }
-    @Given("the following appointments exist in the system:")
-    public void the_following_appointments_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
-    }
+    /**
+	 * @author theodore
+	 * @param string username
+	 * @param string2 combo name
+	 */
     @When("{string} initiates the deletion of service combo {string}")
     public void initiates_the_deletion_of_service_combo(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string combo name
+	 * @param string num appointments
+	 */
     @Then("the number of appointments in the system with service {string} shall be {string}")
     public void the_number_of_appointments_in_the_system_with_service_shall_be(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string num appointemnts
+	 */
     @Then("the number of appointments in the system shall be {string}")
     public void the_number_of_appointments_in_the_system_shall_be(String string) {
         // Write code here that turns the phrase above into concrete actions
@@ -414,11 +461,25 @@ public class CucumberStepDefinitions {
     // UpdateServiceCombo
     //================================================================================
 	
+    /**
+	 * @author theodore
+	 * @param string username
+	 * @param string2 combo name
+	 * @param string3 new combo name
+	 * @param string4 main service
+	 * @param string5 services
+	 * @param string6 mandatory
+	 */
     @When("{string} initiates the update of service combo {string} to name {string}, main service {string} and services {string} and mandatory setting {string}")
     public void initiates_the_update_of_service_combo_to_name_main_service_and_services_and_mandatory_setting(String string, String string2, String string3, String string4, String string5, String string6) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+    /**
+	 * @author theodore
+	 * @param string combo name
+	 * @param string2 new combo name
+	 */
     @Then("the service combo {string} shall be updated to name {string}")
     public void the_service_combo_shall_be_updated_to_name(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions

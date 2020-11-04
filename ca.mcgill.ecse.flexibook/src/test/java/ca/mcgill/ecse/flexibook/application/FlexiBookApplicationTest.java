bbce7926f7bb9e3998963515f0a98c31377802a9
He Qian Wang <heqianw@gmail.com>
2020-11-04 00:27:40 -0500

    Quick fix

--- ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/application/FlexiBookApplicationTest.java
+++ ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/application/FlexiBookApplicationTest.java
@@ -14,18 +14,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 class FlexiBookApplicationTest {
-    @Test void appHasAGreeting() {
-        FlexiBookApplication classUnderTest = new FlexiBookApplication();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
-
-
     private static int nextDriverID = 1;
-	private static String filename = "testdata.btms";
+	private static String filename = "testdata.flexibook";
 	
 	@BeforeAll
 	public static void setUpOnce() {
@@ -34,6 +29,16 @@ class FlexiBookApplicationTest {
 	
 	@BeforeEach
 	public void setUp() {
+		// remove test file
+		File f = new File(filename);
+		f.delete();
+		// clear all data
+		FlexiBook flexibook = FlexiBookApplication.getFlexiBook();
+		flexibook.delete();
+	}
+	
+	@AfterAll
+	public static void tearDownOnce() {
 		// remove test file
 		File f = new File(filename);
 		f.delete();

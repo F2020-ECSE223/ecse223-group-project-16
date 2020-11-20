5dbc534c0621e7131f587faed155f8549ab2f30b
ju-liechen <julie.chen@mail.mcgill.ca>
2020-11-20 14:55:54 -0400

    push before work, added some initializations

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/view/BusinessInfoPage.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/view/BusinessInfoPage.java
@@ -1,7 +1,9 @@
 package ca.mcgill.ecse.flexibook.view;
 
+import ca.mcgill.ecse.flexibook.application.FlexiBookApplication;
 import ca.mcgill.ecse.flexibook.controller.FlexiBookController;
 import ca.mcgill.ecse.flexibook.controller.InvalidInputException;
+import ca.mcgill.ecse.flexibook.model.BusinessHour;
 
 import javax.swing.GroupLayout;
 import javax.swing.JFrame;
@@ -40,20 +42,20 @@ public class BusinessInfoPage extends JFrame {
   private JButton editContactInfoButton;
   	// Business Hour Elements
   private JTable businessHoursContainer;
-  private JTextField monday;
-  private JTextField tuesday;
-  private JTextField wednesday;
-  private JTextField thursday;
-  private JTextField friday;
-  private JTextField saturday;
-  private JTextField sunday;
-  private JTextField mondayHours;
-  private JTextField tuesdayHours;
-  private JTextField wednesdayHours;
-  private JTextField thursdayHours;
-  private JTextField fridayHours;
-  private JTextField saturdayHours;
-  private JTextField sundayHours;
+  private JLabel mon;
+  private JLabel tues;
+  private JLabel wed;
+  private JLabel thurs;
+  private JLabel fri;
+  private JLabel sat;
+  private JLabel sun;
+  private JTextField monHours;
+  private JTextField tuesHours;
+  private JTextField wedHours;
+  private JTextField thursHours;
+  private JTextField friHours;
+  private JTextField satHours;
+  private JTextField sunHours;
   	// Business address & contact
   private JTextField businessAddress;
   private JTextField businessPhone;
@@ -66,6 +68,48 @@ public class BusinessInfoPage extends JFrame {
   private void initComponents(){
     businessInfoLabel = new JLabel();
     businessInfoLabel.setText("Business Info Tab here");
+    // Titles
+    businessNameTitle = new JLabel(FlexiBookApplication.getFlexiBook().getBusiness().getName());
+    businessHoursTitle = new JLabel("Business Hours");
+    businessAddressTitle = new JLabel("Address");
+    businessContactInfoTitle = new JLabel("Contact Information");
+    // Edit buttons
+    editHoursButton = new JButton("Click to edit hours");
+    editAddressButton = new JButton("Click to change address");
+    editContactInfoButton = new JButton("Click to change contact information");
+    // Business Hour elements
+    mon = new JLabel("Monday");
+    tues = new JLabel("Tuesday");
+    wed = new JLabel("Wednesday");
+    thurs = new JLabel("Thursday");
+    fri = new JLabel("Friday");
+    sat = new JLabel("Saturday");
+    sun = new JLabel("Sunday");
+    for (BusinessHour bh : FlexiBookApplication.getFlexiBook().getBusiness().getBusinessHours()) {
+    	if (bh.getDayOfWeek().toString().equals("Monday")) {
+    		monHours = new JTextField(bh.getStartTime().toString() + bh.getEndTime().toString());
+    	}
+    	else if (bh.getDayOfWeek().toString().equals("Tuesday")) {
+       		tuesHours = new JTextField(bh.getStartTime().toString() + bh.getEndTime().toString());
+    	}
+    	else if (bh.getDayOfWeek().toString().equals("Wednesday")) {
+       		wedHours = new JTextField(bh.getStartTime().toString() + bh.getEndTime().toString());
+    	}
+    	else if (bh.getDayOfWeek().toString().equals("Thursday")) {
+       		thursHours = new JTextField(bh.getStartTime().toString() + bh.getEndTime().toString());
+    	}
+    	else if (bh.getDayOfWeek().toString().equals("Friday")) {
+       		friHours = new JTextField(bh.getStartTime().toString() + bh.getEndTime().toString());
+    	}
+    	else if (bh.getDayOfWeek().toString().equals("Saturday")) {
+       		satHours = new JTextField(bh.getStartTime().toString() + bh.getEndTime().toString());
+    	}
+    	else if (bh.getDayOfWeek().toString().equals("Sunday")) {
+       		sunHours = new JTextField(bh.getStartTime().toString() + bh.getEndTime().toString());
+    	}
+    }
+    // Business address and contact
+    
 
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     setTitle("Business Info Tab");

04445f6fa3f3df4ec3a556a221050b02f288cb68
He Qian Wang <heqianw@gmail.com>
2020-11-19 23:52:50 -0500

    Make/update/cancel appointments work as intended

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/view/AppointmentsPage.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/view/AppointmentsPage.java
@@ -1,6 +1,8 @@
 package ca.mcgill.ecse.flexibook.view;
 
 import java.awt.Color;
+import java.io.File;
+
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -9,8 +11,15 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
+import ca.mcgill.ecse.flexibook.application.FlexiBookApplication;
+import ca.mcgill.ecse.flexibook.controller.FlexiBookController;
+import ca.mcgill.ecse.flexibook.controller.InvalidInputException;
+import ca.mcgill.ecse.flexibook.controller.TOAppointment;
+import ca.mcgill.ecse.flexibook.controller.TOBookableService;
+
 public class AppointmentsPage extends JFrame {
   private static final long serialVersionUID = 5454788004189244344L;
+  
   private JLabel makeAppointmentLabel;
   private JLabel updateAppointmentLabel;
   private JLabel cancelAppointmentLabel;
@@ -35,9 +44,6 @@ public class AppointmentsPage extends JFrame {
   private JLabel updateStartTimeLabel;
   private JTextField updateStartTimeTextField;
 
-  private JLabel updateServiceToBookLabel;
-  private JComboBox<String> updateServicesList;
-
   private JLabel cancelAppointmentListLabel;
   private JComboBox<String> cancelAppointmentList;
   
@@ -48,10 +54,38 @@ public class AppointmentsPage extends JFrame {
   private JButton cancelAppointmentButton;
 
   private JLabel errorMessage;
-  
+  private String error = null;
+
   public AppointmentsPage() {
-    refreshData();
+    // below is testing
+    File f = new File("data.flexibook");
+		f.delete();
+    FlexiBookApplication.getFlexiBook();
+
+    try{
+      FlexiBookController.login("owner", "password");
+      FlexiBookController.setUpBusinessInfo("Jimmy G", "WallStreet", "0123456789", "hq@wang.com");
+      FlexiBookController.addNewBusinessHour("Monday", "09:00", "20:00");
+      FlexiBookController.addNewBusinessHour("Tuesday", "09:00", "20:00");
+      FlexiBookController.addNewBusinessHour("Wednesday", "09:00", "20:00");
+      FlexiBookController.addNewBusinessHour("Thursday", "09:00", "20:00");
+      FlexiBookController.addNewBusinessHour("Friday", "09:00", "20:00");
+      FlexiBookController.addNewBusinessHour("Saturday", "09:00", "20:00");
+      FlexiBookController.addNewBusinessHour("Sunday", "09:00", "20:00");
+      FlexiBookController.addService("cut", "10", "0", "0");
+      FlexiBookController.addService("trim", "10", "0", "0");
+
+      FlexiBookController.logout();
+
+      FlexiBookController.createCustomerAccount("test", "password");
+      FlexiBookController.login("test", "password");
+    }
+    catch(InvalidInputException e){
+
+    }
+    // everything above is to be removed
     initComponents();
+    refreshData();
   }
 
   private void initComponents(){
@@ -93,14 +127,10 @@ public class AppointmentsPage extends JFrame {
     updateStartTimeLabel.setText("New Appointment start time");
     updateStartTimeTextField = new JTextField();
 
-    updateServiceToBookLabel = new JLabel();
-    updateServiceToBookLabel.setText("New Appointment service");
-
     cancelAppointmentButton = new JButton();
     cancelAppointmentButton.setText("Cancel Appointment");
 
     makeServicesList = new JComboBox<String>(new String[0]);
-    updateServicesList = new JComboBox<String>(new String[0]);
 
     updateAppointmentList = new JComboBox<String>(new String[0]);
 
@@ -110,23 +140,26 @@ public class AppointmentsPage extends JFrame {
 
     makeAppointmentButton.addActionListener(new java.awt.event.ActionListener() {
       public void actionPerformed(java.awt.event.ActionEvent evt) {
-        makeAppointActionPerformed(evt);
-        new FlexiBookPage().setVisible(true);
-        dispose();
+        makeAppointmentActionPerformed(evt);
+        // new FlexiBookPage().setVisible(true);
+        // dispose();
+        // we'll add another button to return to home later
       }
     });
     updateAppointmentButton.addActionListener(new java.awt.event.ActionListener() {
       public void actionPerformed(java.awt.event.ActionEvent evt) {
-        updateAppointActionPerformed(evt);
-        new FlexiBookPage().setVisible(true);
-        dispose();
+        updateAppointmentActionPerformed(evt);
+        // new FlexiBookPage().setVisible(true);
+        // dispose();
+        // we'll add another button to return to home later
       }
     });
     cancelAppointmentButton.addActionListener(new java.awt.event.ActionListener() {
       public void actionPerformed(java.awt.event.ActionEvent evt) {
-        cancelAppointActionPerformed(evt);
-        new FlexiBookPage().setVisible(true);
-        dispose();
+        cancelAppointmentActionPerformed(evt);
+        // new FlexiBookPage().setVisible(true);
+        // dispose();
+        // we'll add another button to return to home later
       }
     });
 
@@ -140,8 +173,8 @@ public class AppointmentsPage extends JFrame {
     layout.setAutoCreateContainerGaps(true);
     layout.setHorizontalGroup(
       layout.createSequentialGroup()
-        .addComponent(errorMessage)
         .addGroup(layout.createParallelGroup()
+          .addComponent(errorMessage)
           .addComponent(makeAppointmentLabel)
           .addComponent(makeStartDateLabel)
           .addComponent(makeStartDateTextField)
@@ -163,10 +196,6 @@ public class AppointmentsPage extends JFrame {
           .addComponent(makeServicesList)
           .addComponent(updateStartTimeLabel)
           .addComponent(updateStartTimeTextField)
-        )
-        .addGroup(layout.createParallelGroup()
-          .addComponent(updateServiceToBookLabel)
-          .addComponent(updateServicesList)
         )
         .addGroup(layout.createParallelGroup()
           .addComponent(makeAppointmentButton)
@@ -194,13 +223,11 @@ public class AppointmentsPage extends JFrame {
           .addComponent(updateAppointmentListLabel)
           .addComponent(updateStartDateLabel)
           .addComponent(updateStartTimeLabel)
-          .addComponent(updateServiceToBookLabel)
         )
         .addGroup(layout.createParallelGroup()
           .addComponent(updateAppointmentList)
           .addComponent(updateStartDateTextField)
           .addComponent(updateStartTimeTextField)
-          .addComponent(updateServicesList)
           .addComponent(updateAppointmentButton)
         )
         .addComponent(cancelAppointmentLabel)
@@ -218,17 +245,123 @@ public class AppointmentsPage extends JFrame {
   private void refreshData() {
     // refresh the TOAppointments that will be used
     // refresh the services list
+    errorMessage.setText(error);
+    if(error == null || error.isEmpty()){
+      makeStartDateTextField.setText("");
+      makeStartTimeTextField.setText("");
+      updateStartDateTextField.setText("");
+      updateStartTimeTextField.setText("");
+
+      makeServicesList.removeAllItems();
+      makeServicesList.addItem("");
+      for(TOBookableService service: FlexiBookController.getBookableServices()){
+        makeServicesList.addItem(service.getName());
+      }
+
+      updateAppointmentList.removeAllItems();
+      updateAppointmentList.addItem("");
+      try{
+        for(TOAppointment app : FlexiBookController.getAppointments(FlexiBookApplication.getCurrentUser().getUsername())){
+          updateAppointmentList.addItem(
+            String.join(" ", 
+              app.getBookableServiceName(), 
+              app.getStartDate().toString(), 
+              app.getStartTime().toString()
+            )
+          );
+        }
+      }
+      catch(InvalidInputException e){
+        errorMessage.setText(e.getMessage());
+      }
+      
+      cancelAppointmentList.removeAllItems();
+      cancelAppointmentList.addItem("");
+      try{
+        for(TOAppointment app : FlexiBookController.getAppointments(FlexiBookApplication.getCurrentUser().getUsername())){
+          cancelAppointmentList.addItem(
+            String.join(" ", 
+              app.getBookableServiceName(), 
+              app.getStartDate().toString(), 
+              app.getStartTime().toString()
+            )
+          );
+        }
+      }
+      catch(InvalidInputException e){
+        errorMessage.setText(e.getMessage());
+      }
+      pack();
+    }
   }
 
-  private void makeAppointActionPerformed(java.awt.event.ActionEvent evt){
-
+  private void makeAppointmentActionPerformed(java.awt.event.ActionEvent evt){
+    error = null;
+    if(makeServicesList.getSelectedItem().toString().isEmpty()){
+      error = "Please select a service for your appointment.";
+    }
+    else{
+      try{
+        FlexiBookController.makeAppointment(
+          FlexiBookApplication.getCurrentUser().getUsername(), 
+          makeStartDateTextField.getText(), 
+          makeServicesList.getSelectedItem().toString(), 
+          makeStartTimeTextField.getText()
+        );
+      }
+      catch(InvalidInputException e){
+        error = e.getMessage();
+      }
+    }
+    refreshData();
+    pack();
   }
 
-  private void updateAppointActionPerformed(java.awt.event.ActionEvent evt){
-    
+  private void updateAppointmentActionPerformed(java.awt.event.ActionEvent evt){
+    error = null;
+    if(updateAppointmentList.getSelectedItem().toString().isEmpty()){
+      error = "Please select an appointment to update.";
+    }
+    else{
+      try{
+        String[] appData = updateAppointmentList.getSelectedItem().toString().split(" ");
+        FlexiBookController.updateAppointment(
+          FlexiBookApplication.getCurrentUser().getUsername(), 
+          appData[0], 
+          appData[1], 
+          appData[2],
+          updateStartDateTextField.getText(),
+          updateStartTimeTextField.getText()
+        );
+      }
+      catch(InvalidInputException e){
+        error = FlexiBookApplication.getCurrentUser().getUsername();
+      }
+    }
+    refreshData();
+    pack();
   }
 
-  private void cancelAppointActionPerformed(java.awt.event.ActionEvent evt){
-    
+  private void cancelAppointmentActionPerformed(java.awt.event.ActionEvent evt){
+    error = null;
+    if(cancelAppointmentList.getSelectedItem().toString().isEmpty()){
+      error = "Please select an appointment to cancel.";
+    }
+    else{
+      try{
+        String[] appData = cancelAppointmentList.getSelectedItem().toString().split(" ");
+        FlexiBookController.cancelAppointment(
+          FlexiBookApplication.getCurrentUser().getUsername(), 
+          appData[0], 
+          appData[1], 
+          appData[2]
+        );
+      }
+      catch(InvalidInputException e){
+        error = FlexiBookApplication.getCurrentUser().getUsername();
+      }
+    }
+    refreshData();
+    pack();
   }
 }
